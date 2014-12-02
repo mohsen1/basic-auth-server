be865d915c311a6f1c928c66dd21f071cc30c768
Mohsen Azimi <msn.azi@gmail.com>
2014-12-02 12:35:23 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var basicAuth = require('basic-auth');
+var port = process.env.PORT || 3000;
+var username = 'user';
+var password = 'pass';
+
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name ===  username && user.pass === password) {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
+
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/', auth, function (req, res) {
+  res.status(200).body('Authenticated');
+});
+
+
+app.listen(port, console.log);
