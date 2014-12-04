ca84739f0bb0728f04352035a701fbbd885cd2ab
Mohsen Azimi <msn.azi@gmail.com>
2014-12-04 13:33:13 -0800

    Allow Authorization header

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var auth = function (req, res, next) {
 // Enable CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
